2e37d8641e129030432b599573879362112645b6
Rebel7351 <60972049+Rebel7351@users.noreply.github.com>
2021-03-13 09:42:58 +0530

    file add

---
+++ model.py
@@ -0,0 +1,17 @@
+import pickle
+from processing import clean_data
+with open('./model.pickle', 'rb') as f:
+    model = pickle.load(f)
+with open('./vectoriser.pickle', 'rb') as f:
+    vectoriser = pickle.load(f)
+
+
+def predict_class(sms):
+    cleaned_sms = clean_data([sms])
+    vector_sms = vectoriser.transform(cleaned_sms)
+    output = model.predict(vector_sms)
+    return output
+
+
+if __name__ == '__main__':
+    print(predict_class('WINNER!! As a valued network customer you have been selected to receivea £900 prize reward! To claim call 09061701461. Claim code KL341. Valid 12 hours only.'))
